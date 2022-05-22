cd79423ba4035937fbb888922febe2e5f1887801
KVD <kamilvdono@gmail.com>
2022-05-22 08:13:49 +0200

    feat: added MultiDictionaries

---
+++ Runtime/DataStructures/DistinctMultiDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace KVD.Utils.DataStructures
+{
+	public class DistinctMultiDictionary<TKey, TElem> : Dictionary<TKey, HashSet<TElem>>
+	{
+		public DistinctMultiDictionary()
+		{
+		}
+		public DistinctMultiDictionary(IDictionary<TKey, HashSet<TElem>> dictionary) : base(dictionary)
+		{
+		}
+		public DistinctMultiDictionary(IDictionary<TKey, HashSet<TElem>> dictionary, IEqualityComparer<TKey> comparer)
+			: base(dictionary, comparer)
+		{
+		}
+		public DistinctMultiDictionary(IEqualityComparer<TKey> comparer) : base(comparer)
+		{
+		}
+		public DistinctMultiDictionary(int capacity) : base(capacity)
+		{
+		}
+		public DistinctMultiDictionary(int capacity, IEqualityComparer<TKey> comparer) : base(capacity, comparer)
+		{
+		}
+		protected DistinctMultiDictionary(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+		public DistinctMultiDictionary(IEnumerable<KeyValuePair<TKey, HashSet<TElem>>> collection) : base(collection)
+		{
+		}
+		public DistinctMultiDictionary(IEnumerable<KeyValuePair<TKey, HashSet<TElem>>> collection, IEqualityComparer<TKey> comparer) : base(collection, comparer)
+		{
+		}
+
+		public void Add(TKey key, TElem element)
+		{
+			if (!TryGetValue(key, out var elements))
+			{
+				elements = new();
+				Add(key, elements);
+			}
+			elements.Add(element);
+		}
+	}
+}
---
+++ Runtime/DataStructures/MultiDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace KVD.Utils.DataStructures
+{
+	public class MultiDictionary<TKey, TElem> : Dictionary<TKey, List<TElem>>
+	{
+		public MultiDictionary()
+		{
+		}
+		public MultiDictionary(IDictionary<TKey, List<TElem>> dictionary) : base(dictionary)
+		{
+		}
+		public MultiDictionary(IDictionary<TKey, List<TElem>> dictionary, IEqualityComparer<TKey> comparer)
+			: base(dictionary, comparer)
+		{
+		}
+		public MultiDictionary(IEqualityComparer<TKey> comparer) : base(comparer)
+		{
+		}
+		public MultiDictionary(int capacity) : base(capacity)
+		{
+		}
+		public MultiDictionary(int capacity, IEqualityComparer<TKey> comparer) : base(capacity, comparer)
+		{
+		}
+		protected MultiDictionary(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+		public MultiDictionary(IEnumerable<KeyValuePair<TKey, List<TElem>>> collection) : base(collection)
+		{
+		}
+		public MultiDictionary(IEnumerable<KeyValuePair<TKey, List<TElem>>> collection, IEqualityComparer<TKey> comparer) : base(collection, comparer)
+		{
+		}
+
+		public void Add(TKey key, TElem element)
+		{
+			if (!TryGetValue(key, out var elements))
+			{
+				elements = new();
+				Add(key, elements);
+			}
+			elements.Add(element);
+		}
+	}
+}
