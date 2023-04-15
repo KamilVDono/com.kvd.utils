1cbcee68189557fc9474e250ae47ec903945f781
KamilVDono <kamilvdono@gmail.com>
2023-04-15 09:22:39 +0200

    feat: add rendering info

---
+++ Runtime/RenderingInfo.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace KVD.Utils
+{
+	public class RenderingInfo : MonoBehaviour
+	{
+		private bool _isDebugging;
+		
+		private void Update()
+		{
+			if (Keyboard.current[Key.Backquote].wasReleasedThisFrame)
+			{
+				_isDebugging = !_isDebugging;
+			}
+		}
+
+		private void OnGUI()
+		{
+			if (!_isDebugging)
+			{
+				return;
+			}
+			
+			var renderingStateInfo = new GUIContent(
+				$"Graphics API: {SystemInfo.graphicsDeviceType}"+
+				$"Multithreading mode: {SystemInfo.renderingThreadingMode}"+
+#if UNITY_EDITOR
+				$"\nMultithreading: {UnityEditor.PlayerSettings.MTRendering}"+
+				$"\nGraphics jobs: {UnityEditor.PlayerSettings.graphicsJobs}-{UnityEditor.PlayerSettings.graphicsJobMode}"+
+#endif
+#if ENABLE_IL2CPP
+				"\nIL2CPP backed"
+#else
+				"\nMono backed"
+#endif
+				);
+			
+			GUILayout.Box(renderingStateInfo);
+		}
+	}
+}
