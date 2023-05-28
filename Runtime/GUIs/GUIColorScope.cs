a498dc8ccde455b5271ac297c0f2a7b1678d862a
KamilVDono <kamilvdono@gmail.com>
2023-05-28 11:52:09 +0200

    feat: add GUIColorScope

---
+++ Runtime/GUIs/GUIColorScope.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace KVD.Utils.GUIs
+{
+	public readonly struct GUIColorScope : IDisposable
+	{
+		private readonly Color _oldColor;
+		private readonly Color _oldGizmosColor;
+		
+		public GUIColorScope(Color color, bool withGizmos = false)
+		{
+			_oldColor       = GUI.color;
+			GUI.color       = color;
+			_oldGizmosColor = Gizmos.color;
+			if (withGizmos)
+			{
+				Gizmos.color = color;
+			}
+		}
+		
+		public void Dispose()
+		{
+			GUI.color    = _oldColor;
+			Gizmos.color = _oldGizmosColor;
+		}
+	}
+}
