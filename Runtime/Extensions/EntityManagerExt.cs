05b6a1d51f59bbea9d2da98b1681ce5bd0a5fd6d
KamilVDono <kamilvdono@gmail.com>
2023-10-15 16:08:11 +0200

    feat: entity manager extension

---
+++ Runtime/Extensions/EntityManagerExt.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Entities;
+
+namespace KVD.Utils.Extensions
+{
+	public static class EntityManagerExt
+	{
+		public static void AddOrSetComponentData<T>(this EntityManager manager, Entity entity, T componentData) where T : unmanaged, IComponentData
+		{
+			if (manager.HasComponent<T>(entity))
+			{
+				manager.SetComponentData(entity, componentData);
+			}
+			else
+			{
+				manager.AddComponentData(entity, componentData);
+			}
+		}
+
+		public static T GetComponentDataOrDefault<T>(this EntityManager manager, Entity entity) where T : unmanaged, IComponentData
+		{
+			return manager.HasComponent<T>(entity) ? manager.GetComponentData<T>(entity) : default;
+		}
+	}
+}
